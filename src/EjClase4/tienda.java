bd1e2b1aa61b227c85f74f65dba56c51d71f5aab
MValeBM <53920006+MValeBM@users.noreply.github.com>
2019-08-16 12:18:24 -0300

    .

---
+++ src/EjClase4/tienda.java
@@ -0,0 +1,11 @@
+package EjClase4;
+
+public class tienda {
+
+    public static void main(String[] args){
+
+        Movil celular;
+        celular = new Movil(6, 32,15000,32,200);
+        System.out.println(celular.getInfo());
+    }
+}
